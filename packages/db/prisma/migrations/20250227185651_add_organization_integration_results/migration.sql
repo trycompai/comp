08a6409bc3094c1b4286b4ca85dbb26c21fca365
7yntax <kris@trycomp.ai>
2025-03-03 18:36:24 +0000

    COM-20 added assigned user to schema

--- packages/db/prisma/migrations/20250227185651_add_organization_integration_results/migration.sql
+++ packages/db/prisma/migrations/20250227185651_add_organization_integration_results/migration.sql
@@ -8,6 +8,7 @@ CREATE TABLE "Organization_integration_results" (
     "completedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "organizationIntegrationId" TEXT NOT NULL,
     "organizationId" TEXT NOT NULL,
+    "assignedUserId" TEXT NOT NULL,
 
     CONSTRAINT "Organization_integration_results_pkey" PRIMARY KEY ("id")
 );
@@ -18,8 +19,14 @@ CREATE INDEX "Organization_integration_results_organizationIntegrationId_idx" ON
 -- CreateIndex
 CREATE INDEX "Organization_integration_results_organizationId_idx" ON "Organization_integration_results"("organizationId");
 
+-- CreateIndex
+CREATE INDEX "Organization_integration_results_assignedUserId_idx" ON "Organization_integration_results"("assignedUserId");
+
 -- AddForeignKey
 ALTER TABLE "Organization_integration_results" ADD CONSTRAINT "Organization_integration_results_organizationIntegrationId_fkey" FOREIGN KEY ("organizationIntegrationId") REFERENCES "OrganizationIntegrations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Organization_integration_results" ADD CONSTRAINT "Organization_integration_results_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Organization_integration_results" ADD CONSTRAINT "Organization_integration_results_assignedUserId_fkey" FOREIGN KEY ("assignedUserId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
