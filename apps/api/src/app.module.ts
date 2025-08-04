e02a51c6c6330fe3bbb4b0097bf20492c47787a0
Claudio Fuentes <imclaudfuen@gmail.com>
2025-08-03 21:05:03 -0400

    fix: remove duplicate import of HealthModule in app.module.ts
    - Eliminated the redundant import statement for HealthModule in app.module.ts to streamline the code and improve readability.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { HealthModule } from './health/health.module';
 import { OrganizationModule } from './organization/organization.module';
 import { TasksModule } from './tasks/tasks.module';
-import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [AuthModule, OrganizationModule, TasksModule, HealthModule],
