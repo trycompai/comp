a106395a32c7f7519eb5700966e5d5791bb8d901
chasprowebdev <chasgarciaprowebdev@gmail.com>
2025-09-03 21:38:05 -0400

    create Context endpoints

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -10,6 +10,7 @@ import { awsConfig } from './config/aws.config';
 import { HealthModule } from './health/health.module';
 import { OrganizationModule } from './organization/organization.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ContextModule } from './context/context.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { TasksModule } from './tasks/tasks.module';
     }),
     AuthModule,
     OrganizationModule,
+    ContextModule,
     DevicesModule,
     AttachmentsModule,
     TasksModule,
