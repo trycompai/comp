ae6c9cf5c77fe409dc01f99abe178f10b35f63a5
james-miview <james.w@miviewis.com>
2025-09-19 10:09:11 -0500

    chore(BetterAuth): fix better auth command deprecation warning
    to resolve error
    
    [Better Auth]: Your Better Auth config includes advanced.generateId which is deprecated. Please use advanced.database.generateId instead. This will be removed in future releases.

--- apps/portal/src/app/lib/auth.ts
+++ apps/portal/src/app/lib/auth.ts
@@ -11,9 +11,10 @@ export const auth = betterAuth({
     provider: 'postgresql',
   }),
   advanced: {
-    // This will enable us to fall back to DB for ID generation.
-    // It's important so we can use custom IDs specified in Prisma Schema.
-    generateId: false,
+    database: {
+      // Fallback to DB for ID generation to respect Prisma model IDs.
+      generateId: false,
+    },
   },
   trustedOrigins: ['http://localhost:3000', 'https://*.trycomp.ai'],
   secret: process.env.AUTH_SECRET!,
