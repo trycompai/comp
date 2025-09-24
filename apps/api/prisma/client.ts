a50720242ab76d355c4d9bb113c2cb752ace8e52
Mariano Fuentes <marfuen98@gmail.com>
2025-09-24 16:30:36 -0400

    feat(deps): add @prisma/adapter-pg to support PostgreSQL adapter

--- apps/api/prisma/client.ts
+++ apps/api/prisma/client.ts
@@ -1,7 +1,10 @@
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
