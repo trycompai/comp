283a91a004a1c3b16a8713129be282fd65116ccf
chasprowebdev <chasgarciaprowebdev@gmail.com>
2025-11-04 12:20:51 -0500

    fix(app): show alert icon for deactivated users on RecentLogs and comments

--- apps/app/src/app/(app)/[orgId]/policies/[policyId]/data/index.ts
+++ apps/app/src/app/(app)/[orgId]/policies/[policyId]/data/index.ts
@@ -194,6 +194,7 @@ export const getComments = async (policyId: string): Promise<CommentWithAuthor[]
           name: comment.author.user.name,
           email: comment.author.user.email,
           image: comment.author.user.image,
+          deactivated: comment.author.deactivated,
         },
         attachments: attachments.map((att) => ({
           id: att.id,
