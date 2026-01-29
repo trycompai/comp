6119663bcded6c360b252310bfcb0d5ac56aeff7
Andreas Reußner <andreas.reussner@rgigroup.com>
2026-01-29 15:16:01 +0100

    feat(mail): add nodemailer for smtp delivery
    Signed-off-by: Andreas Reußner <andreas.reussner@rgigroup.com>

--- packages/email/lib/mail-service.ts
+++ packages/email/lib/mail-service.ts
@@ -1,14 +1,12 @@
-import { Resend } from 'resend';
+import { CreateEmailOptions, CreateEmailResponse, Resend } from 'resend';
+import { createTransport, Transporter } from 'nodemailer';
+import { render } from '@react-email/render';
+import { MailOptions } from 'nodemailer/lib/smtp-pool';
 
 export const resend = process.env.RESEND_API_KEY ? new Resend(process.env.RESEND_API_KEY) : null;
 
 export interface MailService {
-  send(payload: any): Promise<EmailResponse>;
-}
-
-interface EmailResponse {
-  data: { id: string } | null;
-  error: { message: string } | null;
+  send(payload: CreateEmailOptions): Promise<CreateEmailResponse>;
 }
 
 class ResendMailService implements MailService {
@@ -18,8 +16,35 @@ class ResendMailService implements MailService {
     this.resend = new Resend(apiKey);
   }
 
-  send(payload: any): Promise<EmailResponse> {
+  send(payload: CreateEmailOptions): Promise<CreateEmailResponse> {
     return this.resend.emails.send(payload);
+  }
+}
+
+class RelayMailService implements MailService {
+  private transporter: Transporter;
+
+  constructor(host: string, port: number, user: string, pass: string) {
+    this.transporter = createTransport({
+      host,
+      port,
+      secure: port === 465,
+      auth: {
+        user,
+        pass,
+      },
+    });
+  }
+
+  async send(payload: CreateEmailOptions): Promise<CreateEmailResponse> {
+    const { react } = payload;
+    const html = react ? await render(react) : undefined;
+    const mailOptions: MailOptions = { html, ...payload };
+
+    return this.transporter.sendMail(mailOptions).then(({ id }) => ({
+      data: { id },
+      error: null,
+    })).catch(e => ({ data: null, error: e }));
   }
 }
 
@@ -27,6 +52,18 @@ let mailService: MailService | null = null;
 
 if (process.env.RESEND_API_KEY) {
   mailService = new ResendMailService(process.env.RESEND_API_KEY);
+} else if (
+  process.env.RELAY_SMTP_HOST &&
+  process.env.RELAY_SMTP_PORT &&
+  process.env.RELAY_SMTP_USER &&
+  process.env.RELAY_SMTP_PASS
+) {
+  mailService = new RelayMailService(
+    process.env.RELAY_SMTP_HOST,
+    Number(process.env.RELAY_SMTP_PORT),
+    process.env.RELAY_SMTP_USER,
+    process.env.RELAY_SMTP_PASS,
+  );
 }
 
 export default mailService;
