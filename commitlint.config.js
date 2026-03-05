fbfc6e3389bce3a13043025a380789216645555c
Mariano Fuentes <marfuen98@gmail.com>
2026-03-05 10:15:54 -0500

    chore: remove CODE_OF_CONDUCT and commitlint configuration files

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'body-max-line-length': [0, 'always', Number.POSITIVE_INFINITY], // Disable body line length rule
-        'header-max-length': [0, 'always', Number.POSITIVE_INFINITY], // Disable header line length rule
-    },
-};
