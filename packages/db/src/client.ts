7c548d67ffdf22bc758ab5151338dea302d214cc
Mariano Fuentes <marfuen98@gmail.com>
2026-03-31 11:45:33 -0400

    chore(db): update client/index to use generated prisma-client path

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,11 +1,10 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-  });
+const adapter = new PrismaPg(process.env.DATABASE_URL!);
+
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,2 @@
-export * from '@prisma/client';
+export * from './generated/prisma/client';
 export { db } from './client';
