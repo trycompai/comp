a9d2a8e4863a2c32016418c05f2d79d0a9655680
Mariano Fuentes <marfuen98@gmail.com>
2026-03-31 11:53:34 -0400

    chore(api): add patch-schema-generator script for prisma-client-js in v7

---
+++ apps/api/scripts/patch-schema-generator.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const schemaPath = path.join(__dirname, '../prisma/schema.prisma');
+let schema = fs.readFileSync(schemaPath, 'utf8');
+
+// Replace the generator block (handles both prisma-client and prisma-client-js)
+schema = schema.replace(
+  /generator client \{[^}]*\}/s,
+  `generator client {
+  provider = "prisma-client-js"
+  output   = "./generated"
+}`
+);
+
+fs.writeFileSync(schemaPath, schema);
+console.log('[patch-schema] Set generator to prisma-client-js with output=./generated');
